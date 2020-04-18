7bd1df5686ee062947506464844339d91f6a23d4
Miroslavovic <michalovicm@gmail.com>
2020-04-18 20:33:55 +0200

    faker added to test happy path with some generated data

--- tests/register_new_customer_test.js
+++ tests/register_new_customer_test.js
@@ -1,20 +1,22 @@
+const faker = require('faker'); 
+
 Feature('login');
 
-Scenario('test dummy happy path', (I) => {
+Scenario('test happy path', (I) => {
     I.amOnPage('/index.php?controller=authentication&back=my-account')
-    I.fillField('email_create', 'dummy_test_mirosavovic123456@testNull.cz') //must be unique in dummy version 
+    I.fillField('email_create', faker.internet.exampleEmail())
     I.click('SubmitCreate')
     I.checkOption('//*[@id="id_gender1"]')
-    I.fillField('customer_firstname', 'Pan')
-    I.fillField('customer_lastname', 'Jan')
-    I.fillField('passwd', '12345')
+    I.fillField('customer_firstname', faker.name.firstName())
+    I.fillField('customer_lastname', faker.name.lastName())
+    I.fillField('passwd', faker.internet.password())
     I.selectOption('days', '2')
     I.selectOption('months', '3')
     I.selectOption('years', '1990')
     I.checkOption('//*[@id="newsletter"]')
     I.checkOption('//*[@id="optin"]')
-    I.fillField('firstname', 'Pan')
-    I.fillField('lastname', 'Jan')
+    //I.fillField('firstname', 'Pan')
+    //I.fillField('lastname', 'Jan')
     I.fillField('company', 'Firma')
     I.fillField('address1', 'Happy Place')
     I.fillField('address2', '666')
@@ -22,11 +24,16 @@ Scenario('test dummy happy path', (I) => {
     I.selectOption('//*[@id="id_state"]', 'Kentucky')
     I.fillField('postcode', '41008')
     I.fillField('other', 'Blabla')
-    I.fillField('phone', '123456789')
-    I.fillField('phone_mobile', '1234567890')
+    I.fillField('phone', faker.phone.phoneNumberFormat())
+    I.fillField('phone_mobile', faker.phone.phoneNumberFormat())
     I.fillField('alias', 'My address')
     I.click('submitAccount')
     I.seeCurrentUrlEquals('/index.php?controller=my-account')
+    I.see('Welcome to your account', '.container');
 
     I.wait(5)
 });
+
+
+
+
