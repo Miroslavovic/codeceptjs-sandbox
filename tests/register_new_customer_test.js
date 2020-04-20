c428fd2b8391982d6d50ec1a07036ec243fdde95
Miroslavovic <michalovicm@gmail.com>
2020-04-20 16:24:26 +0200

    'test happy path without unrequired inputs' scenario added

--- tests/register_new_customer_test.js
+++ tests/register_new_customer_test.js
@@ -35,7 +35,28 @@ Scenario('test happy path', (I) => {
     I.see('Welcome to your account', '.container');
 
     I.wait(5)
-});
+})
+
+Scenario('test happy path without unrequired inputs', (I) => {
+    I.amOnPage('/index.php?controller=authentication&back=my-account')
+    I.fillField('email_create', faker.internet.exampleEmail())
+    I.click('SubmitCreate')
+    I.fillField('customer_firstname', faker.name.firstName())
+    I.fillField('customer_lastname', faker.name.lastName())
+    I.fillField('passwd', faker.internet.password())
+    I.fillField('address1', 'Happy Place')
+    I.fillField('city', 'Beaver Dam')
+    I.selectOption('//*[@id="id_state"]', 'Kentucky')
+    I.fillField('postcode', '41008')
+    I.fillField('phone_mobile', faker.phone.phoneNumberFormat())
+    I.fillField('alias', 'My address')
+    I.click('submitAccount')
+    I.seeCurrentUrlEquals('/index.php?controller=my-account')
+    I.see('Welcome to your account', '.container');
+
+    I.wait(5)
+})
+    ;
 
 
 
